07ce2ca5a2fbd899fc4c7037cb1f8f8d5868a6ff
chandradhar rao <chandradhar.rao@gmail.com>
2021-01-10 20:42:10 +0530

    payment react

--- client/src/components/Payment.js
+++ client/src/components/Payment.js
@@ -1,10 +1,37 @@
 function PaymentMode(){
     return(
-        <div className="card">
+        <div className="container">
+            <div className="card">
             <div className="card content">
-                <div class="waves-effect waves-light btn">Google pay</div>
-                <div class="waves-effect waves-light btn">Master Pay</div>
+                <div className="card">
+
+                    <div className="card-content white text">
+
+                        <h1 className="card-title" style={{fontWeight:"bold"}}>Payment Methods : </h1>
+                        <br></br>
+                        <div className="waves-effect waves-light btn"><i className="material-icons left"></i>Google Pay</div>
+                        <br></br>
+                        <br></br>
+                        <div className="waves-effect waves-light btn"><i className="material-icons left"></i>Masterpass</div>
+                        <br></br>
+                        <br></br>
+                        <span className="card-title" style={{fontWeight:"bold"}}>Enter Card Details Below : </span>
+                        <br></br>
+
+                        <label htmlFor="pin"></label>MM/YY CVV
+                        <div className="input-field"><input id = "pin" type="text"/></div>
+
+                        <label htmlFor="Name"></label>Name
+                        <div className="input-field"><input id = "Name" type="text"/></div>
+
+                        <label htmlFor="zip"></label>Zip
+                        <div className="input-field"><input id = "zip" type="text"/></div>
+
+                        <div className="waves-effect waves-light btn">Pay</div>
+                    </div>
+                </div>
             </div>
+        </div>
         </div>
     )
 }
