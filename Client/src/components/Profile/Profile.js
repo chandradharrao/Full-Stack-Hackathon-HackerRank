b43dec1dcdf27da87e4e1df4837f46e0cccf8c94
Dhruval360 <dhruvalpb@gmail.com>
2021-01-23 19:51:17 +0530

    Revamped Profile Page

--- Client/src/components/Profile/Profile.js
+++ Client/src/components/Profile/Profile.js
@@ -1,23 +1,33 @@
 import {useContext} from 'react';
 import { UserDetailsContext } from '../../App';
+import "./Profile.css"
 
 function Profile(){
     const {state,dispatch} = useContext(UserDetailsContext);
     
     return(
-        <div className = "container">
-            <div className="card" id="profile-card">
-                <div className = "card-header" id="profile-header">
-                    <img src = {state.imgURL} alt="profile pic" ></img>
+        <div className = "profile-container">
+            <div className="profile-wrapper">
+                <div className="profile-card">
+                    <div className="profile-header">
+                        <div className = "profile-img">
+                            <img src = {state.imgURL} alt="profile pic" ></img>
+                        </div>
+                    </div>
+                    <div className="profile-content">
+                        <h1 id="profile-name">{state.name}</h1>
+                        <div className="profile-data">
+                            <h4>Organization: {state.orgName}</h4>
+                            <h4>Registration Date : {state.regDate}</h4>
+                            <h4>Employee ID: {state.empID}</h4>
+                            <h4>Registration ID : {state.regID}</h4>
+                            <h4>Email: {state.email}</h4>
+                            <h4>Mobile numer: {state.mobNo}</h4>
+                        </div>
+                    </div>
                 </div>
-                <div className="card-content" id="profile-content">
-                    <h3>Welcome {state.name}!</h3>
-                    <h4>{state.orgName}</h4>
-                    <h4>Registration Date : {state.regDate}</h4>
-                    <h4>{state.empID}</h4>
-                    <h4>Registration ID : {state.regID}</h4>
-                    <h4>{state.email}</h4>
-                    <h4>{state.mobNo}</h4>
+                <div className="recent-card">
+                    <h1 id="profile-name">Recent Orders</h1>
                 </div>
             </div>
         </div>
